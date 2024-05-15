3433d8f137fd6d65e368cb2766f50e794d334653
Kasundi Vemalya Sudusinghe <kasundivemalya29@gmail.com>
2024-05-15 21:44:08 +0530

    Created a seperate file called shape.py

--- math.py
+++ math.py
@@ -6,10 +6,3 @@ def subtract(x, y):
     return x - y
 
 
-class Shape:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
